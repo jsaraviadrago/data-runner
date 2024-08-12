e3c600c3ea78043afb99e82d08229ebbde24a6a2
JC Saravia <jsaraviadrago@gmail.com>
2024-08-12 17:58:13 -0500

    creating the web app

--- sl1.py
+++ sl1.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+
+url = ""
+
+data_runs = pd.read_csv("")
+
+
 st.header("My webapp")
 x = st.slider("Select a value")
 st.write(x, "squared is", x * x)
+
