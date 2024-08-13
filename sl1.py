5c1bb4c91834191d8917ff44e4d3850f56c9ef46
JC Saravia <jsaraviadrago@gmail.com>
2024-08-13 10:28:10 -0500

    sl1.py editing graphs

--- sl1.py
+++ sl1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sn
+
 
 url_data = "https://raw.githubusercontent.com/jsaraviadrago/data-runner/main/Activities_07082024_VF.csv"
 
