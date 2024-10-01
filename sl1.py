e7462b26f0a9b13c2ddda4d30a79848c82cc0513
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2024-10-01 06:56:04 -0500

    Update

--- sl1.py
+++ sl1.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import plotly.express as px
 
 url_data = "https://raw.githubusercontent.com/jsaraviadrago/data-runner/main/Activities_07082024_VF.csv"
 
